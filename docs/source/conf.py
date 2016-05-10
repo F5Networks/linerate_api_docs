bb6d39a65fd09594224c46f46e33f155be8d8a02
Jodie Putrino <j.putrino@f5.com>
2016-05-10 16:24:12 -0600

    added requirements file, added recommonmark configs to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,10 +33,18 @@ extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+
+# Use recommonmark to use Markdown and RST in the same project
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.md'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
